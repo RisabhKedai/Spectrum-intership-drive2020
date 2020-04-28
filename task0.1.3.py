c853e9ee17d370c64c3d8102d37b483e5d79f120
RisabhKedai <58504602+RisabhKedai@users.noreply.github.com>
2020-04-28 16:13:24 +0530

    Create task0.1.3.py
    Given a number count the total number of digits in a number

---
+++ task0.1.3.py
@@ -0,0 +1,5 @@
+#accepting usre input
+n=input()
+#though it is a number the input takes it as a string since it is not wrapped in int()
+print(len(n))
+#the len() gives the no. of characters hence the number of digits in the given number
